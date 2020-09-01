126f3da040d0db312a4b945af3b94edc23cf672f
waltercalderon-udevs <walter.calderon@u-devs.com>
2020-09-01 12:21:04 +0600

    Redux integration part 2

---
+++ src/constants/error-msg-definitions.js
@@ -0,0 +1,14 @@
+const language = {
+    en: {
+        invalidCred: 'Invalid credentials',
+        warning: 'Warning',
+        error: 'Error',
+        success: 'Success',
+        registerSuccess: 'Registration success',
+        loginSuccess: 'Login success',
+        postFailed: 'Form post failed',
+        validationError: 'Invalid field(s)',
+    }
+}
+
+export default language;
