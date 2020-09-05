ce6371454c149739665c45376ee20451cd5e9a6c
andresmata <amata@xtensifi.com>
2020-09-05 17:25:34 -0600

    Login/Registry cleanup

--- src/Components/Public/Login.js
+++ src/Components/Public/Login.js
@@ -10,101 +10,95 @@ import ROUTES from '../../util/routes'
 
 class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            form: {}
-        }
-
-        SimpleReactValidator.addLocale('es', {
-            required: 'este campo es requerido',
-            email: 'Introduzca un correo electrónico válido'
-        });
-
-        this.validator = new SimpleReactValidator({
-            locale: 'es',
-            autoForceUpdate: this
-        });
+    this.state = {
+      form: {email: '', password: ''}
     }
 
-    inputChangeHandler = (e) => {
-        let obj = this.state.form
-        obj[e.target.name] = e.target.value
-        this.setState({form: obj})
-        // console.log(this.state.form);
+    SimpleReactValidator.addLocale('es', {
+      required: 'este campo es requerido',
+      email: 'Introduzca un correo electrónico válido'
+    });
+
+    this.validator = new SimpleReactValidator({
+      locale: 'es',
+      autoForceUpdate: this
+    });
+  }
+
+  inputChangeHandler = (e) => {
+    this.setState({...this.state.form[e.target.name] = e.target.value})
+  }
+
+  formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (this.validator.allValid()) {
+      this.processSubmit();
+    } else {
+      this.validator.showMessages();
     }
+  };
 
-    formSubmitHandler = (e) => {
-        e.preventDefault();
-        if (this.validator.allValid()) {
-            this.processSubmit();
-        } else {
-            this.validator.showMessages();
-        }
-    };
+  gotoStep = (e, step) => {
+    this.props.redirectToUrl(step)
+  }
 
-    gotoStep = (e, step) => {
-        this.props.redirectToUrl(step)
-    }
+  componentWillMount() {
+    this.props.isLoggedInAndRedirect()
+  }
 
-    componentWillMount() {
-        this.props.isLoggedInAndRedirect()
-    }
-
-    processSubmit() {
-        let {form} = this.state
-        // console.log(form)
-        this.props.postLoginForm(form)
-    }
+  processSubmit() {
+    let {form} = this.state
+    this.props.postLoginForm(form)
+  }
 
 
-    render() {
-        return (
-            <>
-                <div className="container-login">
-                    <img src={Logo} alt="website logo"/>
-                    <form onSubmit={this.formSubmitHandler}>
-                        <div className="ls-panel">
-                            <h3>Iniciar sesión</h3>
-                            <input type="text" className="user" name="email" placeholder="Correo electrónico"
-                                   onChange={this.inputChangeHandler} value={this.state.form.email}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('email', this.state.form.email, 'required|email')}
-                            </p>
-                            <input type="password" className="pass" name="password" placeholder="Contraseña"
-                                   onChange={this.inputChangeHandler} value={this.state.form.password}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('password', this.state.form.password, 'required')}
-                            </p>
-                            <Button className="btn btn-theme" type="submit">
-                                INGRESAR
-                            </Button>
-                            <div className="link-holder" style={{marginTop: '20px'}}><a className="float-left"
-                                                                                        onClick={e => this.gotoStep(e, ROUTES.FORGOT_PASSWORD)}>Olvidaste
-                                tu contraseña?</a>&emsp;<a className="float-right"
-                                                           onClick={e => this.gotoStep(e, ROUTES.REGISTRY)}>Registrarse</a>
-                            </div>
-                        </div>
-                    </form>
+  render() {
+    return (
+        <>
+          <div className="container-login">
+            <img src={Logo} alt="website logo"/>
+            <form onSubmit={this.formSubmitHandler}>
+              <div className="ls-panel">
+                <h3>Iniciar sesión</h3>
+                <input type="text" className="user" name="email" placeholder="Correo electrónico"
+                       onChange={this.inputChangeHandler} value={this.state.form.email}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('email', this.state.form.email, 'required|email')}
+                </p>
+                <input type="password" className="pass" name="password" placeholder="Contraseña"
+                       onChange={this.inputChangeHandler} value={this.state.form.password}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('password', this.state.form.password, 'required')}
+                </p>
+                <Button className="btn btn-theme" type="submit">
+                  INGRESAR
+                </Button>
+                <div className="link-holder" style={{marginTop: '20px'}}>
+                  <a className="float-left" onClick={e => this.gotoStep(e, ROUTES.FORGOT_PASSWORD)}>
+                    Olvidaste tu contraseña?</a>
+                  <a className="float-right" onClick={e => this.gotoStep(e, ROUTES.REGISTRY)}>Registrarse</a>
                 </div>
-
-
-            </>
-        );
-    }
+              </div>
+            </form>
+          </div>
+        </>
+    );
+  }
 }
 
 const mapStateToProps = ({auth}) => ({
-    auth
+  auth
 })
 
 const mapDispatchToProps = dispatch =>
     bindActionCreators(
         {
-            postLoginForm,
-            isLoggedInAndRedirect,
-            redirectToUrl
+          postLoginForm,
+          isLoggedInAndRedirect,
+          redirectToUrl
         },
         dispatch
     )
--- src/Components/Public/Registry.js
+++ src/Components/Public/Registry.js
@@ -5,182 +5,181 @@ import SimpleReactValidator from 'simple-react-validator';
 import Logo from "../../assets/images/logo-kuai-white.svg";
 import Modal from "react-bootstrap/Modal";
 import TermsAndCondition from "./TermsAndCondition";
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { postFormData, isLoggedInAndRedirect, redirectToUrl } from '../../actions'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {postFormData, isLoggedInAndRedirect, redirectToUrl} from '../../actions'
 import ROUTES from '../../util/routes'
 
 class Registry extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            form: {},
-            step: 'REGISTER',
-            show: false,
-            registrySuccess: false,
+    this.state = {
+      form: {name: '', restaurantName: '', email: '', password: ''},
+      step: 'REGISTER',
+      show: false,
+      registrySuccess: false,
+    }
+
+    SimpleReactValidator.addLocale('es', {
+      required: 'este campo es requerido',
+      email: 'Introduzca un correo electrónico válido'
+    });
+
+    this.validator = new SimpleReactValidator({
+      locale: 'es',
+      autoForceUpdate: this,
+      validators: {
+        mixPass: {
+          message: 'Esta no es una contraseña válida',
+          rule: (val, params, validator) => {
+            // console.log(val, params, validator);
+            return validator.helpers.testRegex(val, /^(?=.*\d)(?=.*[ A-Za-z]).{8,}$/)
+          },
         }
+      }
+    });
+  }
 
-        SimpleReactValidator.addLocale('es', {
-            required: 'este campo es requerido',
-            email: 'Introduzca un correo electrónico válido'
-        });
+  gotoStep = (e, step) => {
+    this.props.redirectToUrl(step)
+  }
 
-        this.validator = new SimpleReactValidator({
-            locale: 'es',
-            autoForceUpdate: this,
-            validators: {
-                mixPass: {
-                    message: 'Esta no es una contraseña válida',
-                    rule: (val, params, validator) => {
-                        // console.log(val, params, validator);
-                        return validator.helpers.testRegex(val, /^(?=.*\d)(?=.*[ A-Za-z]).{8,}$/)
-                    },
-                }
-            }
-        });
+  componentWillMount() {
+    this.props.isLoggedInAndRedirect()
+  }
+
+  showTermsAndConditionsModal = () => {
+    this.setState({show: true});
+  }
+
+  hideTermsAndConditionsModal = () => {
+    this.setState({show: false});
+  }
+
+  hideRegistrySuccessModal = () => {
+    this.setState({registrySuccess: false});
+  }
+
+  inputChangeHandler = (e) => {
+    let obj = this.state.form
+    obj[e.target.name] = e.target.value
+    this.setState({form: obj})
+    // console.log(this.state.form);
+  }
+
+  formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (this.validator.allValid()) {
+      this.processSubmit();
+    } else {
+      this.validator.showMessages();
     }
+  };
 
-    gotoStep = (e, step) => {
-        this.props.redirectToUrl(step)
-    }
+  processSubmit() {
+    let data = this.state
+    // console.log(data)
+    this.props.postFormData(data)
+  }
 
-    componentWillMount() {
-        this.props.isLoggedInAndRedirect()
-    }
-
-    showTermsAndConditionsModal = () => {
-        this.setState({show: true});
-    }
-
-    hideTermsAndConditionsModal = () => {
-        this.setState({show: false});
-    }
-
-    hideRegistrySuccessModal = () => {
-        this.setState({registrySuccess: false});
-    }
-
-    inputChangeHandler = (e) => {
-        let obj = this.state.form
-        obj[e.target.name] = e.target.value
-        this.setState({ form: obj })
-        // console.log(this.state.form);
-    }
-
-    formSubmitHandler = (e) => {
-        e.preventDefault();
-        if (this.validator.allValid()) {
-            this.processSubmit();
-        } else {
-            this.validator.showMessages();
-        }
-    };
-
-    processSubmit() {
-        let data = this.state
-        // console.log(data)
-        this.props.postFormData(data)
-    }
-
-    render() {
-        return (
-            <>
-                <div className="container-login">
-                    <img src={Logo} alt="website logo"/>
-                    <form onSubmit={this.formSubmitHandler}>
-                        <div className="ls-panel">
-                            <h3>Registro</h3>
-                            <input type="text" placeholder="Nombre" name="name" onChange={this.inputChangeHandler}
-                                   value={this.state.form.name}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('name', this.state.form.name, 'required')}
-                            </p>
-                            <input type="text" placeholder="Restaurante" name="restaurantName"
-                                   onChange={this.inputChangeHandler} value={this.state.form.restaurantName}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('restaurantName', this.state.form.restaurantName, 'required')}
-                            </p>
-                            <input type="text" placeholder="Correo electrónico" name="email"
-                                   onChange={this.inputChangeHandler}
-                                   value={this.state.form.email}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('email', this.state.form.email, 'required|email')}
-                            </p>
-                            <input type="password" placeholder="Contraseña" name="password"
-                                   onChange={this.inputChangeHandler} value={this.state.form.password}/>
-                            <p style={{color: "red"}}>
-                                {this.validator.message('password', this.state.form.password, 'required|mixPass')}
-                            </p>
-                            <p className="help-txt">Minimo 8 caracteres, letras y números unicamente.</p>
-                            <Button className="btn btn-theme" type="submit">
-                                REGISTRARSE
-                            </Button>
-                            <div className="link-holder">¿Ya tienes cuenta? <a onClick={e => this.gotoStep(e, ROUTES.LOGIN)}>Iniciar
-                                sesión</a>
-                            </div>
-                            <div className="link-holder" style={{marginTop: '10px'}}>
-                                Al registrarse usted acepta nuestros <Link className="default"
-                                                                           onClick={this.showTermsAndConditionsModal}>Terminos
-                                y
-                                Condiciones</Link>
-                            </div>
-                        </div>
-                    </form>
+  render() {
+    return (
+        <>
+          <div className="container-login">
+            <img src={Logo} alt="website logo"/>
+            <form onSubmit={this.formSubmitHandler}>
+              <div className="ls-panel">
+                <h3>Registro</h3>
+                <input type="text" placeholder="Nombre" name="name" onChange={this.inputChangeHandler}
+                       value={this.state.form.name}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('name', this.state.form.name, 'required')}
+                </p>
+                <input type="text" placeholder="Restaurante" name="restaurantName"
+                       onChange={this.inputChangeHandler} value={this.state.form.restaurantName}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('restaurantName', this.state.form.restaurantName, 'required')}
+                </p>
+                <input type="text" placeholder="Correo electrónico" name="email"
+                       onChange={this.inputChangeHandler}
+                       value={this.state.form.email}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('email', this.state.form.email, 'required|email')}
+                </p>
+                <input type="password" placeholder="Contraseña" name="password"
+                       onChange={this.inputChangeHandler} value={this.state.form.password}/>
+                <p style={{color: "red"}}>
+                  {this.validator.message('password', this.state.form.password, 'required|mixPass')}
+                </p>
+                <p className="help-txt">Minimo 8 caracteres, letras y números unicamente.</p>
+                <Button className="btn btn-theme" type="submit">
+                  REGISTRARSE
+                </Button>
+                <div className="link-holder">¿Ya tienes cuenta? <a onClick={e => this.gotoStep(e, ROUTES.LOGIN)}>Iniciar
+                  sesión</a>
                 </div>
+                <div className="link-holder" style={{marginTop: '10px'}}>
+                  Al registrarse usted acepta nuestros
+                  <a className="default" onClick={this.showTermsAndConditionsModal}>
+                    Terminos y Condiciones</a>
+                </div>
+              </div>
+            </form>
+          </div>
 
-                <Modal
-                    className="cstm-modal"
-                    size="xl"
-                    show={this.state.show}
-                    onHide={this.hideTermsAndConditionsModal}
-                    aria-labelledby="contained-modal-title-vcenter"
-                    centered
-                    scrollable={true}
-                >
-                    <Modal.Header closeButton>
-                        <Modal.Title id="example-modal-sizes-title-sm">
+          <Modal
+              className="cstm-modal"
+              size="xl"
+              show={this.state.show}
+              onHide={this.hideTermsAndConditionsModal}
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+              scrollable={true}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="example-modal-sizes-title-sm">
 
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <TermsAndCondition/>
-                    </Modal.Body>
-                </Modal>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <TermsAndCondition/>
+            </Modal.Body>
+          </Modal>
 
-                <Modal
-                    className="cstm-modal"
-                    size="sm"
-                    show={this.state.registrySuccess}
-                    onHide={this.hideRegistrySuccessModal}
-                    aria-labelledby="contained-modal-title-vcenter"
-                    centered
-                >
-                    <Modal.Header closeButton>
-                        <Modal.Title id="example-modal-sizes-title-sm">
+          <Modal
+              className="cstm-modal"
+              size="sm"
+              show={this.state.registrySuccess}
+              onHide={this.hideRegistrySuccessModal}
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="example-modal-sizes-title-sm">
 
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        Gracias por registrarse con nosotros, hemos enviado un email de verificación a la direccion
-                        que ingresaste.
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Gracias por registrarse con nosotros, hemos enviado un email de verificación a la direccion
+              que ingresaste.
 
-                        Sigue la instrucciones que te enviamos para completar tu perfil y comenzar a utilizar la
-                        plataforma.
-                    </Modal.Body>
-                </Modal>
-            </>
-        );
-    }
+              Sigue la instrucciones que te enviamos para completar tu perfil y comenzar a utilizar la
+              plataforma.
+            </Modal.Body>
+          </Modal>
+        </>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch =>
     bindActionCreators(
         {
-            postFormData,
-            isLoggedInAndRedirect,
-            redirectToUrl
+          postFormData,
+          isLoggedInAndRedirect,
+          redirectToUrl
         },
         dispatch
     )
