993e6f7c48b4e8da1574dab5cb4d1c1f2f7365c6
waltercalderon-udevs <walter.calderon@u-devs.com>
2020-09-09 15:36:08 +0600

    Worked on trello issues (show hide pass on icon click)

--- src/Components/Public/Registry.js
+++ src/Components/Public/Registry.js
@@ -78,6 +78,15 @@ class Registry extends React.Component {
     }
   };
 
+  showHidePassword = () => {
+    const x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
   processSubmit() {
     let data = this.state
     // console.log(data)
@@ -108,8 +117,16 @@ class Registry extends React.Component {
                 <p style={{color: "red"}}>
                   {this.validator.message('email', this.state.form.email, 'required|email')}
                 </p>
-                <input type="password" placeholder="Contraseña" name="password"
+                <div className="pass-icon">
+                <input type="password" placeholder="Contraseña" name="password" id="password"
                        onChange={this.inputChangeHandler} value={this.state.form.password}/>
+                <svg className="color-switch active" width="16" height="12" viewBox="0 0 16 12" fill="none"
+                     xmlns="http://www.w3.org/2000/svg" onClick={this.showHidePassword}>
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M8 0.363647C4.36364 0.363647 1.25818 2.62547 0 5.81819C1.25818 9.01092 4.36364 11.2727 8 11.2727C11.6364 11.2727 14.7418 9.01092 16 5.81819C14.7418 2.62547 11.6364 0.363647 8 0.363647ZM8 9.45456C5.99273 9.45456 4.36364 7.82547 4.36364 5.81819C4.36364 3.81092 5.99273 2.18183 8 2.18183C10.0073 2.18183 11.6364 3.81092 11.6364 5.81819C11.6364 7.82547 10.0073 9.45456 8 9.45456ZM5.81818 5.81819C5.81818 4.61092 6.79273 3.63637 8 3.63637C9.20727 3.63637 10.1818 4.61092 10.1818 5.81819C10.1818 7.02547 9.20727 8.00001 8 8.00001C6.79273 8.00001 5.81818 7.02547 5.81818 5.81819Z"
+                        fill="#660ADE"/>
+                </svg>
+                </div>
                 <p style={{color: "red"}}>
                   {this.validator.message('password', this.state.form.password, 'required|mixPass')}
                 </p>
