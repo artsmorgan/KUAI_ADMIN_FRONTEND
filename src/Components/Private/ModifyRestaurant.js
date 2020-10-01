b3cf757350a17f786f9ffa9ecc85dcdc81be89df
waltercalderon-udevs <walter.calderon@u-devs.com>
2020-10-01 11:23:56 +0600

    url

--- src/Components/Private/ModifyRestaurant.js
+++ src/Components/Private/ModifyRestaurant.js
@@ -884,6 +884,17 @@ class ModifyRestaurant extends React.Component {
                                                        placeholder="LINK AL MENU"
                                                        onChange={this.inputChangeHandler}
                                                        value={this.state.dataToPost.tinyUrl} readOnly id="menuLink"/>
+
+                                                <div className="url_input">
+                                                    <p>Awesome URL</p>
+                                                    <button className="btn-copy">
+                                                    <svg className="svg-icon" viewBox="0 0 20 20" style={{width: "30px", cursor: "pointer"}} onClick={this.copyFunction}
+                                                                                     title={"Copy to clipboard"}>
+                                                    <path
+                                                        d="M17.391,2.406H7.266c-0.232,0-0.422,0.19-0.422,0.422v3.797H3.047c-0.232,0-0.422,0.19-0.422,0.422v10.125c0,0.232,0.19,0.422,0.422,0.422h10.125c0.231,0,0.422-0.189,0.422-0.422v-3.797h3.797c0.232,0,0.422-0.19,0.422-0.422V2.828C17.812,2.596,17.623,2.406,17.391,2.406 M12.749,16.75h-9.28V7.469h3.375v5.484c0,0.231,0.19,0.422,0.422,0.422h5.483V16.75zM16.969,12.531H7.688V3.25h9.281V12.531z"></path>
+                                                    </svg>
+                                                    </button>
+                                                </div>
                                                 {/*<p className-="error-txt" style={{color: "red"}}>
                                                     {this.validator.message('menuLink', this.state.dataToPost.menuLink, 'required')}
                                                 </p>*/}
